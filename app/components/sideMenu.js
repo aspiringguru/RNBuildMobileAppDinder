d5e612717fd5689fcbb57b39104ca3eec233892a
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-26 19:21:45 +1000

    ch 2-5 completed. side menu added

--- app/components/sideMenu.js
+++ app/components/sideMenu.js
@@ -14,7 +14,40 @@ export default class SideMenu extends Component {
   constructor(props) {
     super(props)
   }
+
+  /*
+  *   this.props.navigator.replace("Match")
+  *   where "Match" is title of the scene to render.
+  *   this.props.navigator.replace("History") : use replace instead of pushes
+  *   prevents user from navigating backwards with arrow
+  *
+  *   theme={this.props.theme} : sets theme to theme passed in with props.
+  *   <Header/> = blank header, will populate later
+  *   item.onPress.bind(this) = bind item.onPress to this
+  */
   render() {
-    return null
-  }
-}
+    let list = [{
+      title: "Match",
+      onPress: () => {
+        this.props.navigator.replace("Match")
+      }
+    }, {
+      title: "History",
+      onPress: () => {
+        this.props.navigator.replace("History")
+      }
+    }]
+    return (
+      <Container theme={this.props.theme}>
+        <Header/>
+        <View>
+          <List dataArray={list} renderRow={(item) =>
+            <ListItem button onPress={item.onPress.bind(this)}>
+              <Text> {item.title} </Text>
+            </ListItem>
+          }/>
+        </View>
+      </Container>
+    )/* end return */
+  }/* end render */
+}/* end export default class SideMenu extends Component */
